bbc20f0fabcf5c321b561da0118125e2589c2811
mac <mac@MRLi.local>
2016-01-03 19:56:03 +0800

    test1

---
+++ Learning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Learning
+//
+//  Created by mac on 15/12/13.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
